9ba5a2f28cbc142f2cf09c621e211c0852a9b3d6
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-11-09 23:40:43 +0600

    fix midlewares

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
 const middlewares = [
-	cors({ credentials: true, origin: "https://ndas-39655.web.app" }),
-	cookieParser(),
-	express.static("public"),
-	express.json(),
-	express.urlencoded({ extended: true }),
+    cors({
+        credentials: true,
+        origin: ["https://ndas-39655.web.app", "http://localhost:3000"],
+    }),
+    cookieParser(),
+    express.static("public"),
+    express.json(),
+    express.urlencoded({ extended: true }),
 ];
 
 module.exports = (app) => {
-	app.use(middlewares);
+    app.use(middlewares);
 };
