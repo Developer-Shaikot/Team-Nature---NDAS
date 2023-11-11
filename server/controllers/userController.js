012e3480df60bf0f666af68570a3c9ba5962583d
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-11-11 20:00:16 +0600

    fix logout

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,74 +4,74 @@ const User = require("../models/UserModel");
 const { generateToken, verifyToken } = require("../controllers/jwtController");
 
 exports.registerUser = expressAsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const isEmailExits = await User.exists({ email });
+    const isEmailExists = await User.exists({ email });
 
-  if (isEmailExits) {
-    return res.status(401).json({
-      success: false,
-      message: "Email already in use",
+    if (isEmailExists) {
+        return res.status(401).json({
+            success: false,
+            message: "Email already in use",
+        });
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const newUser = await new User({ ...req.body, password: hashedPass }).save();
+
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        user: newUser,
     });
-  }
-
-  const hashedPass = await bcrypt.hash(password, 10);
-
-  const newUser = await new User({ ...req.body, password: hashedPass }).save();
-
-  res.status(201).json({
-    success: true,
-    message: "User created successfully",
-    user: newUser,
-  });
 });
 
 exports.loginUser = expressAsyncHandler(async (req, res) => {
-	const { email, password } = req.body;
+    const { email, password } = req.body;
 
-	const user = await User.findOne({ email }).select("name email user_type +password");
+    const user = await User.findOne({ email }).select("name email user_type +password");
 
-	if (user) {
-		const isPswMatch = await bcrypt.compare(password, user.password);
+    if (user) {
+        const isPswMatch = await bcrypt.compare(password, user.password);
 
-		if (isPswMatch) {
-			const token = generateToken({
-				id: user._id,
-				email: user.email,
-				user_type: user.user_type,
-			});
+        if (isPswMatch) {
+            const token = generateToken({
+                id: user._id,
+                email: user.email,
+                user_type: user.user_type,
+            });
 
-			// res.cookie("userId", user._id);
-			res.cookie("userId", user._id, {
-				maxAge: 6 * 60 * 60 * 1000, // 6 hour
-				secure: process.env.NODE_ENV === "production" ? true : false,
-				sameSite: process.env.NODE_ENV === "production" ? "none" : false,
-			});
+            res.cookie("userId", user._id, {
+                maxAge: 6 * 60 * 60 * 1000, // 6 hours
+                secure: process.env.NODE_ENV === "production" ? true : false,
+                sameSite: process.env.NODE_ENV === "production" ? "none" : false,
+            });
 
-			// set httpOnly auth cookie to browser
-			res.cookie("auth", token, {
-				maxAge: 26 * 60 * 60 * 1000, // 6 hour
-				secure: process.env.NODE_ENV === "production" ? true : false,
-				httpOnly: process.env.NODE_ENV === "production" ? true : false,
-				sameSite: process.env.NODE_ENV === "production" ? "none" : false,
-			});
+            // set httpOnly auth cookie to the browser
+            res.cookie("auth", token, {
+                maxAge: 26 * 60 * 60 * 1000, // 26 hours
+                secure: process.env.NODE_ENV === "production" ? true : false,
+                httpOnly: process.env.NODE_ENV === "production" ? true : false,
+                sameSite: process.env.NODE_ENV === "production" ? "none" : false,
+            });
 
-			return res.status(200).json({
-				success: true,
-				message: "Login successful!",
-				token,
-			});
-		}
-		return res.status(401).json({
-			success: false,
-			message: "Wrong credentials!",
-		});
-	}
+            return res.status(200).json({
+                success: true,
+                message: "Login successful!",
+                token,
+            });
+        }
 
-	res.status(401).json({
-		success: false,
-		message: "Wrong credentials!",
-	});
+        return res.status(401).json({
+            success: false,
+            message: "Wrong credentials!",
+        });
+    }
+
+    res.status(401).json({
+        success: false,
+        message: "Wrong credentials!",
+    });
 });
 
 exports.logoutUser = expressAsyncHandler(async (req, res) => {
@@ -81,99 +81,96 @@ exports.logoutUser = expressAsyncHandler(async (req, res) => {
         httpOnly: process.env.NODE_ENV === "production" ? true : false,
         sameSite: process.env.NODE_ENV === "production" ? "none" : false,
     });
+
     res.status(200).json({
         success: true,
         message: "Logout successful",
     });
 });
 
-
 exports.isLoggedIn = expressAsyncHandler(async (req, res) => {
-  if (req.cookies && req.cookies.auth) {
-    return res.status(200).json({
-      status: true,
-      user: verifyToken(req.cookies.auth),
-    });
-  }
+    if (req.cookies && req.cookies.auth) {
+        return res.status(200).json({
+            status: true,
+            user: verifyToken(req.cookies.auth),
+        });
+    }
 
-  return res.status(200).json({
-    status: false,
-    user: null,
-  });
+    return res.status(200).json({
+        status: false,
+        user: null,
+    });
 });
 
 exports.getLoggedInUser = expressAsyncHandler(async (req, res) => {
-  const token = req.cookies.auth;
-  const user = verifyToken(token);
+    const token = req.cookies.auth;
+    const user = verifyToken(token);
 
-  if (user) {
-    const userInfo = await User.findById(user.id);
-    return res.status(200).json({
-      status: true,
-      user: userInfo,
+    if (user) {
+        const userInfo = await User.findById(user.id);
+        return res.status(200).json({
+            status: true,
+            user: userInfo,
+        });
+    }
+
+    res.status(200).json({
+        status: false,
+        user: null,
     });
-  }
-
-  res.status(200).json({
-    status: false,
-    user: null,
-  });
 });
 
 exports.isTeacher = expressAsyncHandler(async (req, res) => {
-  const user = verifyToken(req.cookies?.auth);
+    const user = verifyToken(req.cookies?.auth);
 
-  if (user) {
-    const userInfo = await User.findById(user.id);
-    return res.status(200).json({
-      status: true,
-      user: userInfo,
+    if (user) {
+        const userInfo = await User.findById(user.id);
+        return res.status(200).json({
+            status: true,
+            user: userInfo,
+        });
+    }
+
+    res.status(200).json({
+        status: false,
+        user: null,
     });
-  }
-
-  res.status(200).json({
-    status: false,
-    user: null,
-  });
-
-  // if (user) {
-  // 	const userInfo = await User.findById(user.id);
-  // 	return res.status(200).json({
-  // 		status: true,
-  // 		user: userInfo,
-  // 	});
-  // }
-
-  // res.status(200).json({
-  // 	status: false,
-  // 	user: null,
-  // });
 });
 
 exports.addAdmin = expressAsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const isEmailExits = await User.exists({ email });
+    const isEmailExists = await User.exists({ email });
 
-  if (isEmailExits) {
-    return res.status(401).json({
-      success: false,
-      message: "Email already in use",
+    if (isEmailExists) {
+        return res.status(401).json({
+            success: false,
+            message: "Email already in use",
+        });
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const newAdmin = await new User({ ...req.body, password: hashedPass }).save();
+
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        user: newAdmin,
     });
-  }
-
-  const hashedPass = await bcrypt.hash(password, 10);
-
-  const newAdmin = await new User({ ...req.body, password: hashedPass }).save();
-
-  res.status(201).json({
-    success: true,
-    message: "User created successfully",
-    user: newUser,
-  });
 });
 
 exports.logoutController = (req, res) => {
-  res.clearCookie("auth");
-  res.status(200).json({ success: true, message: "Logout successful" });
+    res.cookie("auth", "", {
+        maxAge: -1,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        httpOnly: process.env.NODE_ENV === "production" ? true : false,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : false,
+    });
+
+    if (req.cookies.auth) {
+        return res.status(500).json({ message: "Logout failed", success: false });
+    }
+
+    res.status(200).json({ success: true, message: "Logout successful" });
 };
